8b0699ef32273cc205de65a5374e9d20e5134f2b
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-08 13:47:02 +0000

    challenge sorted

--- js/my_script.js
+++ js/my_script.js
@@ -1,11 +1,9 @@
 //will assign css class .selection to the odd numbered table rows
 $(document).ready(function() {
-    // $(".selection").css("background-color", "#ee7d57");
-    // $("tr:odd").addClass("selection");
-    $(".selection").css("background-color", "#E84610");
-    $("tr:even").addClass("selection");
-    $(".selection").css("background-color", "#ee7d57");
     $("tr:odd").addClass("selection");
+    $("tr:even").addClass("selectionEven");
 });
 
-// Why does the order in which I add the color means that I will get two different coloured rows or they all will look the same?
+
+// light red: #ee7d57
+//  red: #E84610
