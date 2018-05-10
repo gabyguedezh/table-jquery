5479c909637c613b8cbc2ccd5968ed709470e944
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-10 21:01:49 +0000

    modified the html and changed the js to start a new challenge within the traversing lesson. struggling to solve it

--- js/my_script.js
+++ js/my_script.js
@@ -1,9 +1,49 @@
-//will assign css class .selection to the odd numbered table rows
+// //will assign css class .selection to the odd numbered table rows
+// $(document).ready(function() {
+//     $("tr:odd").addClass("selection");
+//     $("tr:even").addClass("selectionEven");
+// });
+
+
+// // light red: #ee7d57
+// //  red: #E84610
+
 $(document).ready(function() {
-    $("tr:odd").addClass("selection");
-    $("tr:even").addClass("selectionEven");
+
+    // var firstRow = $("tr").first();
+
+    // $(firstRow).on("click", function() {
+
+    //     var highlightCells = $("tr:first").children();
+
+    //     $(highlightCells).addClass("selection");
+    // });
+
+    $("th").on("click", function() {
+        if ($(this).parent().css("background-color") == "#c65353") {
+            $(this).parent().removeClass("selection");
+        } else {
+            $(this).parent().addClass("selection");
+        }
+    });
+
 });
 
 
-// light red: #ee7d57
-//  red: #E84610
+// var colHead = ("table").find("th");
+
+// $(colHead).on("click", function() {
+//     $(this).addClass("selection");
+// });
+
+//     $().on("click", function() {
+//     $("th").addClass("selection");
+// });
+
+// $("th").on("click", function(){
+
+//     // var should be the corresponding column
+//     var highlightCol = $(this).parent();
+
+//     $(highlightCol).addClass("selection");
+// });
